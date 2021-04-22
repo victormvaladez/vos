dc30388aa9757407c6007f6ab7d6914e34e29172
Victor Valadez <victor.m.valadez@gmail.com>
2021-04-21 22:16:22 -0700

    switch to 32-bit protected mode

--- bootloader/bootsector.asm
+++ bootloader/bootsector.asm
@@ -22,18 +22,59 @@ call print_hex
 mov dx, [0x9000 + 512]
 call print_hex
 
+call switch_to_pm
+jmp $
 
-infinite_loop:
-	jmp infinite_loop
+switch_to_pm:
+	lgdt [gdt_descriptor]
+	cli
+	mov eax, cr0 ; set first bit of CR0 (control register) to 1 to enter into protected mode
+	or eax, 0x1  ;
+	mov cr0, eax ;
 
+	jmp CODE_SEG:init_protected_mode
+
+; ---- real mode routines
 %include "print_string.asm"
 %include "print_hex.asm"
 %include "disk.asm"
+%include "gdt.asm"
+
+; ---- protected mode routines
+[bits 32]
+
+%include "print_string_pm.asm"
+
+init_protected_mode:
+	
+	mov ax, DATA_SEG  ; now in 32bit mode, redefine all segments
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+
+	mov ebp, 0x90000  ; reset the stack position
+	mov esp, ebp
+
+	call begin_protected_mode
+
+	jmp $
+
+begin_protected_mode:
+	xor ebx, ebx
+	mov ebx, pm_message
+	call print_string_pm
+
+	jmp $
+
 
 ; Global variables
 some_data: db "z"
 
 my_string: db "Hello world! ", 0
+
+pm_message: db "Entered into Protected mode.", 0
 
 boot_drive: db 0
 
